9bf5e67b34540d911b64fac607f3125f0611225c
Rashid2006 <aryakubu010@st.ug.edu.gh>
2024-05-26 15:37:16 +0000

    Commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text style={styles.text}>my name is <Text style={styles.name}>Yakubu Abdul Rashid</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'brown',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 24,
+  },
+  name: {
+    fontWeight: 'bold',
+  }
 });
